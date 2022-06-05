e993384a07374dc02c7209cc046b119e08332dd7
rileyMoriarty <hermionesn4@gmail.com>
2022-06-05 22:27:34 +0800

    001

---
+++ PDF/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿
+namespace PDF
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnExport = new System.Windows.Forms.Button();
+            this.lblname = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnExport
+            // 
+            this.btnExport.Location = new System.Drawing.Point(313, 579);
+            this.btnExport.Name = "btnExport";
+            this.btnExport.Size = new System.Drawing.Size(124, 63);
+            this.btnExport.TabIndex = 0;
+            this.btnExport.Text = "匯出PDF檔案";
+            this.btnExport.UseVisualStyleBackColor = true;
+            this.btnExport.Click += new System.EventHandler(this.btnExport_Click_1);
+            // 
+            // lblname
+            // 
+            this.lblname.AutoSize = true;
+            this.lblname.Font = new System.Drawing.Font("微軟正黑體", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.lblname.Location = new System.Drawing.Point(154, 119);
+            this.lblname.Name = "lblname";
+            this.lblname.Size = new System.Drawing.Size(87, 43);
+            this.lblname.TabIndex = 1;
+            this.lblname.Text = "姓名";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微軟正黑體", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(154, 213);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 43);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "年紀";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("微軟正黑體", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.Location = new System.Drawing.Point(154, 297);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 43);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "星座";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("微軟正黑體", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.textBox1.Location = new System.Drawing.Point(266, 122);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(237, 43);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("微軟正黑體", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.textBox2.Location = new System.Drawing.Point(266, 216);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(237, 43);
+            this.textBox2.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("微軟正黑體", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.textBox3.Location = new System.Drawing.Point(266, 305);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(237, 43);
+            this.textBox3.TabIndex = 6;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("微軟正黑體", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.textBox4.Location = new System.Drawing.Point(266, 391);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(481, 43);
+            this.textBox4.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("微軟正黑體", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label3.Location = new System.Drawing.Point(154, 383);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 43);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "住址";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("微軟正黑體", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label4.Location = new System.Drawing.Point(154, 461);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(87, 43);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "電話";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Font = new System.Drawing.Font("微軟正黑體", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.textBox5.Location = new System.Drawing.Point(266, 472);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(312, 43);
+            this.textBox5.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("微軟正黑體", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label5.Location = new System.Drawing.Point(320, 28);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(155, 43);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "基本資料";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(785, 727);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblname);
+            this.Controls.Add(this.btnExport);
+            this.Name = "Form1";
+            this.Text = "winform介面轉PDF";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExport;
+        private System.Windows.Forms.Label lblname;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ PDF/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.IO;
+using iTextSharp.text.pdf;
+using VisioForge.MediaFramework.Helpers;
+
+namespace PDF
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        public static string GetExportPDFPath()
+        {
+            string filename = System.DateTime.Now.ToString().Replace("/", "").Replace(":", "") + "XXX";
+            var exportFileName = filename + ".pdf";
+            var saveFileDialog = new SaveFileDialog { FileName = exportFileName };
+            saveFileDialog.Filter = "匯出資料檔案(*.pdf)|*.pdf";
+            saveFileDialog.FilterIndex = 0;
+            saveFileDialog.RestoreDirectory = true;
+            saveFileDialog.Title = "匯出資料檔案";
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                return saveFileDialog.FileName;
+            }
+            else
+            {
+                return String.Empty;
+            }
+        }
+        public void ConvertJPGToPDF(string jpgfile, string pdf)
+        {
+            iTextSharp.text.Rectangle PageSize = new iTextSharp.text.Rectangle(this.Width, this.Height);
+            var document = new iTextSharp.text.Document(PageSize, 25, 25, 25, 25);
+            //var document = new iTextSharp.text.Document(new iTextSharp.text.Rectangle(), 25, 25, 25, 25);
+            using (var stream = new FileStream(pdf, FileMode.Create, FileAccess.Write, FileShare.None))
+            {
+                PdfWriter.GetInstance(document, stream);
+                document.Open();
+                using (var imageStream = new FileStream(jpgfile, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+                {
+                    var image = iTextSharp.text.Image.GetInstance(imageStream);
+                    if (image.Height > PageSize.Height - 25)
+                    {
+                        image.ScaleToFit(PageSize.Width - 25, PageSize.Height - 25);
+                    }
+                    else if (image.Width > PageSize.Width - 25)
+                    {
+                        image.ScaleToFit(PageSize.Width - 25, PageSize.Height - 25);
+                    }
+                    image.Alignment = iTextSharp.text.Image.ALIGN_MIDDLE;
+                    document.Add(image);
+                }
+                document.Close();
+            }
+        }
+
+        private void btnExport_Click_1(object sender, EventArgs e)
+        {
+            //GDI+
+            //this.groupBox1.Visible = false;
+            //this.label1.Focus();
+            Bitmap bmp = new Bitmap(this.Size.Width, this.Size.Height);
+            this.DrawToBitmap(bmp, new System.Drawing.Rectangle(0, 0, Width, Height));
+            bmp.Save(".//temp.jpg");
+            //選擇匯出pdf的路徑並匯出 ITextSharp.dll外掛
+            string pdfpath = GetExportPDFPath();
+            if (pdfpath == String.Empty)
+            {
+                MessageBox.Show("匯出失敗，請重試!!");
+                //this.groupBox1.Visible = true;
+                return;
+            }
+            else
+            {
+                ConvertJPGToPDF(".//temp.jpg", pdfpath);
+                MessageBox.Show("匯出成功!!");
+                this.Close();
+            }
+        }
+    }
+}
